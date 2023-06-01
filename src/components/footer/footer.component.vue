99182642309f6247dcad289f6dd9e63a99dbb6fe
123ang <angjinsheng@gmail.com>
2023-06-01 11:56:58 +0800

    updatees

---
+++ src/components/footer/footer.component.vue
@@ -0,0 +1,72 @@
+<template>
+    <footer class="footer-section">
+        <div class="container">
+           <div>{{ $t('Copyright') }} {{ (new Date()).getFullYear() }} @ Finance Vanguard </div>
+            <div>
+                Contact us
+                <ul>
+                    <li>Instagram</li>
+                    <li>Email</li>
+                    <li>LinkedIn</li>
+                    <li>Youtube</li>
+                    <li>Facebook</li>
+                    <li>小红书 </li>
+                    <li>TikTok</li>
+                </ul>
+            </div>
+            <div>
+                Links
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Services</li>
+                    <li>Contact Us</li>
+                    <li>Testimonials</li>
+                    <li>Blog</li>
+                </ul>
+            </div>
+        
+        </div>
+    </footer>
+</template>
+
+<script>
+
+export default {
+    name: 'Footer',
+    props: {},
+    components: {},
+    methods: {
+    },
+    mounted() { 
+        const linkedInTip = this.$refs.linkedInTip;
+        new bootstrap.Tooltip(linkedInTip, {
+            title: "LinkedIn",
+            placement: "top",
+        });
+
+        const igTip = this.$refs.igRef;
+        new bootstrap.Tooltip(igTip, {
+            title: "Instagram",
+            placement: "top",
+        });
+        
+        const fbTip = this.$refs.fbRef;
+        new bootstrap.Tooltip(fbTip, {
+            title: "Facebook",
+            placement: "top",
+        });
+        
+        const xhsTip = this.$refs.xhsRef;
+        new bootstrap.Tooltip(xhsTip, {
+            title: "小红书",
+            placement: "top",
+        });
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+    @import '@/assets/css/main.scss';
+    @import './footer.styles.scss';
+</style>
---
+++ src/components/navbar/navbar.component.vue
@@ -0,0 +1,88 @@
+<template>
+    <div class="navbar-section ">
+        <div class="container">
+            <a href="/" class="logo">
+              <img src="@/assets/images/logo/logo.png" alt="logo" height="40" width="182">
+            </a>
+            <div class="icon-container">
+                <div class="close-icon"><i class="fas fa-times"></i></div>
+                <a class="icon close-button" href="">Home</a> 
+                <a class="icon close-button" href="">About</a> 
+                <a class="icon close-button" href="">Services</a> 
+                <a class="icon close-button" href="">Contact Us</a> 
+                <a class="icon close-button" href="">Testimonials</a> 
+                <a class="icon close-button" href="">Blog</a> 
+                <div class="icon language ">
+                    <i class="bi bi-translate"></i>
+                    <div class="dropdown">
+                        <div class="dropdown-item close-button" @click="switchLanguage('en')">English</div>
+                        <div class="dropdown-item close-button" @click="switchLanguage('cn')">中文</div>
+                        <div class="dropdown-item close-button" @click="switchLanguage('my')">Malay</div>
+                    </div>                 
+                </div> 
+                <div class="icon language mobile close-button">
+                  <div class="display">                      
+                    <i class="bi bi-translate"></i>
+                    <div class="display-text">English</div>
+                  </div>
+                </div>
+            </div>            
+            <div class="menu-icon">
+              <i class="fas fa-bars"></i>              
+            </div>
+        </div>
+        <div class="mobile-language-container">
+            <div class="dropdown mobile">
+                <div class="dropdown-item close-button" @click="switchLanguage('en')">English</div>
+                <div class="dropdown-item close-button" @click="switchLanguage('cn')">中文</div>
+                <div class="dropdown-item close-button" @click="switchLanguage('my')">Malay</div>
+            </div>   
+        </div>
+    </div>
+</template>
+
+<script>
+
+
+import { computed } from 'vue';
+import { useI18n } from 'vue-i18n';
+import { useStore } from 'vuex';
+
+
+export default {
+  name: 'Navbar',
+  props: {},
+  components: {
+    CustomModal,
+  },
+  methods: {
+    switchLanguage(lang) {
+      this.$i18n.locale = lang;
+      this.$store.dispatch("setLanguage", lang);
+    },
+    
+  },
+  computed: {
+   
+  },
+  setup() {
+    const store = useStore();
+    const { locale } = useI18n();
+    const selectedLanguage = computed({
+      get: () => locale.value,
+      set: (value) => store.dispatch('setLanguage', value), 
+    });
+
+    return {
+      selectedLanguage,
+    };
+  },
+  mounted() {
+    
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
